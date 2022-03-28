cab42b12712d4dcbddaa2f2573712a5b5eb5aedf
Vitor Raul Bim <vitorrb36@gmail.com>
2022-03-28 12:18:29 -0300

    Projeto Finalizado

---
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/PedidoApiTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Features.Tests;
+using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.MVC.Models;
+using NerdStore.WebApp.Tests.Config;
+using Xunit;
+
+namespace NerdStore.WebApp.Tests
+{
+    [TestCaseOrderer("Features.Tests.PriorityOrderer", "Features.Tests")]
+    [Collection(nameof(IntegrationApiTestsFixtureCollection))]
+    public class PedidoApiTests
+    {
+        private readonly IntegrationTestsFixture<StartupApiTests> _testsFixture;
+
+        public PedidoApiTests(IntegrationTestsFixture<StartupApiTests> testsFixture)
+        {
+            _testsFixture = testsFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar item em novo pedido"), TestPriority(1)]
+        [Trait("Categoria", "Integração API - Pedido")]
+        public async Task AdicionarItem_NovoPedido_DeveRetornarComSucesso()
+        {
+            // Arrange
+            var itemInfo = new ItemViewModel
+            {
+                Id = new Guid("da7d91da-8320-463f-ba14-b224ff4d337a"),
+                Quantidade = 2
+            };
+
+            await _testsFixture.RealizarLoginApi();
+            _testsFixture.Client.AtribuirToken(_testsFixture.UsuarioToken);
+
+            // Act
+            var postResponse = await _testsFixture.Client.PostAsJsonAsync("api/carrinho", itemInfo);
+
+            // Assert
+            postResponse.EnsureSuccessStatusCode();
+        }
+
+        [Fact(DisplayName = "Remover item em pedido existente"), TestPriority(2)]
+        [Trait("Categoria", "Integração API - Pedido")]
+        public async Task RemoverItem_PedidoExistente_DeveRetornarComSucesso()
+        {
+            // Arrange
+            var produtoId = new Guid("da7d91da-8320-463f-ba14-b224ff4d337a");
+            await _testsFixture.RealizarLoginApi();
+            _testsFixture.Client.AtribuirToken(_testsFixture.UsuarioToken);
+
+            // Act
+            var deleteResponse = await _testsFixture.Client.DeleteAsync($"api/carrinho/{produtoId}");
+
+            // Assert
+            deleteResponse.EnsureSuccessStatusCode();
+        }
+    }
+}
